03cb1efee23eb7553afba1cf6b83d0b5f25bbb60
kuljeet-td <kuljeet.s@tatrasdata.com>
2023-04-17 14:01:12 +0530

    changes for random batches ih model

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ LR = 3e-4
 EPOCH = 3
 # DEVICE = 'cuda:1' if torch.cuda.is_available() else 'cpu'
 DEVICE = 'cpu'
-BATCH_SIZE=1
+BATCH_SIZE=16
 
 END_KEY = "### End"
 INSTRUCTION_KEY = "### Instruction:"
 RESPONSE_KEY_NL = f"### Response:\n"
+INPUT_KEY = "### Input:"
 START_TOKEN = "<|startoftext|>"
--- model_notrainer.py
+++ model_notrainer.py
@@ -7,7 +7,7 @@ from datasets import load_dataset
 import torch
 import torch.nn.functional as F
 
-from config import LR, DEVICE, END_KEY, RESPONSE_KEY_NL, INSTRUCTION_KEY, BATCH_SIZE, EPOCH, START_TOKEN
+from config import LR, DEVICE, END_KEY, RESPONSE_KEY_NL, INSTRUCTION_KEY, BATCH_SIZE, EPOCH, START_TOKEN, INPUT_KEY
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
@@ -20,8 +20,9 @@ model = AutoModelForCausalLM.from_pretrained(
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
 tokenizer.bos_token = START_TOKEN
-tokenizer.pad_token = '<|pad|>'
-tokenizer.add_special_tokens({"additional_special_tokens": [END_KEY, INSTRUCTION_KEY, RESPONSE_KEY_NL]})
+tokenizer.pad_token = END_KEY
+tokenizer.eos_token = END_KEY
+tokenizer.add_special_tokens({"additional_special_tokens": [END_KEY, INSTRUCTION_KEY, RESPONSE_KEY_NL, INPUT_KEY]})
 
 model.to(DEVICE)
 
@@ -35,6 +36,9 @@ response_key_stripped = RESPONSE_KEY_NL.strip()
 dataset = dataset.filter(lambda rec: not rec["text"].strip().endswith(response_key_stripped))
 
 def _func(rec):
+    rec["text"] += f"\n\n{END_KEY}"
+    rec['label'] = RESPONSE_KEY_NL + rec['text'].split(RESPONSE_KEY_NL)[1]
+    rec['text'] = rec['text'].split(RESPONSE_KEY_NL)[0]
     rec["text"] = START_TOKEN + rec["text"] + f"\n\n{END_KEY}"
     return rec
 
@@ -59,24 +63,13 @@ dataset = dataset.map(
 
 def _fun_tokenize_labels(rec):
     tokenized_ = tokenizer(
-        RESPONSE_KEY_NL
-    )['input_ids'][0]
-    # seggregate input_ids and label
-    input_ids = []
-    attention_mask = []
-    label = []
-    for i in range(len(rec['input_ids'])):
-        if rec['input_ids'][i] != tokenized_:
-            input_ids.append(rec['input_ids'][i])
-            attention_mask.append(rec['attention_mask'][i])
-        else:
-            label += rec['input_ids'][i:]
-            break
-    rec = {}
-    rec['input_ids'] = torch.tensor(input_ids, dtype=int)
-    rec['attention_mask'] = torch.tensor(attention_mask, dtype=int)
-    rec['label'] = torch.tensor(label, dtype=int)
-    
+        rec["label"],
+        max_length=128,
+        truncation=True,
+        padding='max_length',
+        return_tensors='pt'
+    )
+    rec['label'] = tokenized_['input_ids'][0]
     return rec
     
 dataset = dataset.map(_fun_tokenize_labels)
@@ -86,23 +79,24 @@ dataset_split = dataset.train_test_split(test_size=1000)
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
 def get_random_batch(dataset, batch_size=BATCH_SIZE):
-    random_indexes = torch.randint(0, len(dataset), batch_size)
+    random_indexes = torch.randint(0, len(dataset), (batch_size, ))
     return dataset[random_indexes]
 
 count = 0
 step_at = 10
 with torch.cuda.amp.autocast():
     for ep in range(EPOCH):
-        for row in tqdm(dataset_split['train']):
+        for _ in tqdm(range(len(dataset_split['train'])//BATCH_SIZE)):
+            row = get_random_batch(dataset_split['train'], batch_size=BATCH_SIZE)
             optimizer.zero_grad()
-            if len(row["input_ids"]) <= 1:
-                continue
             batch_in = {
                 "input_ids": torch.tensor(row['input_ids']),
                 "attention_mask": torch.tensor(row['attention_mask'])
             }
             out = model.forward(**batch_in,)
             out = out.logits
+            b, t, c = out.shape
+            out = out.view(b*t, c)
             y_ = torch.tensor(row['label']).flatten()
             loss = F.cross_entropy(out, y_)
             print(loss)
@@ -114,9 +108,5 @@ with torch.cuda.amp.autocast():
             if count % step_at == 0 or loss < 0.8:
                 print("*"*100)
                 print(tokenizer.decode(torch.argmax(out, dim=1)))
-                print("-"*100)
-                print(tokenizer.decode(torch.tensor(row['input_ids'])))
-                print("-"*100)
-                print(tokenizer.decode(torch.tensor(row['label']).flatten()))
             
 print("yolo")
